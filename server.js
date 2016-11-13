f845c0babc28132c67eb3d4ced0be6b929d6a34c
Marc Fournier <marc.fournier91@gmail.com>
2016-11-12 20:38:01 -0500

    Adds api endpoints and route to view file

--- server.js
+++ server.js
@@ -16,5 +16,58 @@ app.use(bodyParser.urlencoded({'extended' : 'true'})); //let body parser handle 
 app.use(bodyParser.json({type: 'application/vnd.api+json'})); // parse application/vnd.api+json as json
 app.use(methodOverride());
 
+var Todo = mongoose.model('Todo', {
+  text : String
+});
+
+//api
+//get all todos
+app.get('/api/todos', function(req, res){
+
+  //use mongoose to get all todos in the database
+  Todo.find(function(err, todos){
+    if (err){
+      res.send(err);
+    }
+    res.json(todos); //return todos in json format
+  });
+});
+
+//create todo and send back all todos after creation
+app.post('/api/todos', function(req, res){
+
+  //create a todo, info comes from AJAX request from angular
+  Todo.create({
+    text : req.body.text,
+    done : false
+  }, function(err, todo){
+    if (err){ res.send(err);}
+
+    //get and return all todos after creation
+    Todo.find(function(err, todos){
+      if (err){ res.send(err);}
+      res.json(todos);
+    });
+
+  });
+});
+
+//delete todo
+app.delete('/api/todos/:todo_id', function(req, res){
+  Todo.remove({
+    _id : req.params.todo_id
+  }, function(err, todo){
+    if (err){ res.send(err)};
+
+    //get and return all todos after deletion
+    Todo.find(function(err, todos){
+      if (err){ res.send(err)};
+      res.json(todos);
+    });
+
+  });
+});
+
+//hey, listen
 app.listen(8080);
 console.log("App listening on port 8080");
